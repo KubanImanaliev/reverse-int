4e19a16b75d02aca6819899ca57837b607fe113e
Kuban <imanaliev.kuban@gmail.com>
2022-01-11 19:45:53 +0600

    greate function for reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+ function reverse (n) {
+     n=n<0?n*-1:n;
+return +n.toString().split("").reverse().join("");
 }
+reverse(456789);
+
+module.exports = reverse;
